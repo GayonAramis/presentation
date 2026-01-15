80b4884084f9c73516442d3753e99736cba780f7
ictcompetencestudentno5 <ictcompetencestudentno5@gmail.com>
2026-01-16 02:30:40 +0800

    16/01/2026

--- testfile.js
+++ testfile.js
@@ -1,30 +1,11 @@
-class Character {
-    constructor(name, health, strength) {
-        this.name = name;
-        this.health = health;
-        this.strength = strength;
+const nameInput = document.getElementById("input-name");
+const displayName = document.getElementById("display-name");
+
+nameInput.addEventListener('input', function() {
+    if (nameInput.value === "") {
+        displayName.textContent = "";
+    } else {
+        displayName.textContent = nameInput.value;
     }
 
-    //method for taking damage
-    takeDamage(amount) {
-        this.health -= amount;
-        console.log(`${this.name} now has ${this.health} HP left.`);
-    }
-}
-
-const player = new Character("Hero", 100, 20);
-const enemy = new Character("Enemy", 80, 15);
-
-console.log(player);
-console.log(enemy);
-
-function attack() {
-    enemy.takeDamage(player.strength);
-    player.takeDamage(enemy.strength);
-}
-
-function defense() {
-    console.log("enemy used Attack")
-    console.log("player used defense, No damage taken this turn.");
-}
-
+});
